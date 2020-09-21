83b62e32ffc0b71fd76d7c0d956cee0c965261f1
Emil <emilpppe@gmail.com>
2020-09-21 23:17:57 +0200

    Add project files.

---
+++ UWP_Uppgift4/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWP_Uppgift4.Services;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_Uppgift4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+
+        }
+
+        private static readonly string _conn = "HostName=EC-iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=Wpp+e1YG/15ffM0XYiK61eeSSVT6ltskDc97X7EktEk=";
+
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+        }
+    }
+}
---
+++ UWP_Uppgift4/Services/DeviceService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibraries.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Uppgift4.Services
+{
+    public static class DeviceService
+    {
+
+
+        private static readonly Random rnd = new Random();
+
+
+
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(20, 30),
+                    Humidity = rnd.Next(40, 50)
+                };
+
+
+
+                //JSON = {"temperature": 20, "humidity": 44}
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+
+
+                
+
+            }
+        }
+    }
+
